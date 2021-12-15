47e0625e31216033ddcb3edbd9c0d009a6645640
Durgesh094 <durgeshchavan1994@gmail.com>
2021-12-15 10:22:23 +0530

    Commiting changes as per suggestions

--- src/com/bridgelabz/deckofcards/DeckofCards.java
+++ src/com/bridgelabz/deckofcards/DeckofCards.java
@@ -4,29 +4,40 @@ import java.util.*;
 
 /* Welcome on Program
   Program Name :- Deck of Cards
-  logic of unique cards 
+  logic of generating unique cards 
  */
 public class DeckofCards {
-	private int rank;
-	private int suit;
-
-	public DeckofCards(int rank, int suit) {
-		this.rank = rank;
-		this.suit = suit;
-	}
-
-	public String toString() {
-		String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
-		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
-		String s = ranks[this.rank] + " of " + suits[this.suit];
-		return s;
-	}
+	private String rank;
+	private String suit;
 
 	public static void main(String[] args) {
-		DeckofCards card = new DeckofCards(13, 1);
-		{
-			System.out.println(card);
+		System.out.println("$$ Welcome in the Game: Deck of Cards $$");
+		DeckofCards cards = new DeckofCards();
+		cards.startGame();
+	}
+
+// initialization of Game with Deck of cards
+	public void startGame() {
+		int[] card = new int[52];
+		String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+
+		for (int i = 0; i < card.length; i++) {
+			card[i] = i;
 		}
 
+// for getting unique card
+		for (int i = 0; i < card.length; i++) {
+			int index = (int) (Math.random() * card.length);
+			card[i] = card[index];
+		}
+
+// for Displaying card
+		for (int i = 0; i < 52; i++) {
+			String suit = suits[card[i] / 13];
+			String rank = ranks[card[i] % 13];
+			System.out.println(rank + "of" + suit);
+
+		}
 	}
 }
